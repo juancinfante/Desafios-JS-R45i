f4869cc445bbca7893bf90929aefcbb7c041b227
juancinfante <juaninfantejj@gmail.com>
2023-04-12 00:44:08 -0300

    Desafios completados

---
+++ 05. VALIDADOR DE CONSTRASEÑAS/script.js
@@ -0,0 +1,13 @@
+let password = prompt("Ingrese contraseña")
+
+// Valido la contraseña con length y metodos REGEX  
+// Debe tener al menos 8 caracteres de longitud. => password.toString().length >= 8
+// Debe contener al menos una letra minúscula y una letra mayúscula. => [A-Z]/.test(password.toString() && /[a-z]/.test(password.toString()
+// Debe contener al menos un número. => /[0-9]/.test(password.toString())
+
+if(password.toString().length >= 8 &&  /[A-Z]/.test(password.toString()) && /[a-z]/.test(password.toString()) && /[0-9]/.test(password.toString())){
+  console.log("Contraseña Valida")
+}else{
+  console.log("Contraseña Invalida")
+}
+
